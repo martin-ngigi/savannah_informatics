13fbfaf9097020f3730195bffb07bc0b36bee6ce
martin-ngigi <martinwainaina001@gmail.com>
2024-02-19 18:59:53 +0300

    Changed DB to Sqlite

--- project/project/settings.py
+++ project/project/settings.py
@@ -139,12 +139,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # SQLite Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # # MYSQL DB.. 
 # NB : MYSQL database not working properly due to allauth package.
